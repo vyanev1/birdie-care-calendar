ba23ff4099f6648c8247e403cf9db34242e47abe
Victor Yanev <56009006+vyanev1@users.noreply.github.com>
2021-02-28 18:19:20 +0200

    Update index.ts

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -3,8 +3,7 @@ var mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com",
     user: "test-read",
-    //password: process.env.DB_PW,
-    password: "xnxPp6QfZbCYkY8",
+    password: process.env.DB_PW,
     database: "birdietest"
 });
 
